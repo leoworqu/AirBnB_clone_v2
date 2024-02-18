1314b91c45740f491fc440b6b13ba0d110cd83bd
leoworqu <leoworqu@gmail.com>
2024-02-18 07:51:17 -0800

    bsss

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,6 @@
 
 html_content="<html><body><h1>Hello, world!</h1></body></html>"
 html_file="/data/web_static/releases/test/index.html"
-location="/etc/nginx/sites-enabled/default"
 
 sudo apt update
 sudo apt install nginx
@@ -18,6 +17,6 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/index index.html;/a \\n\ \ location /hbnb_static {\n \ \ \ alias /data/web_static/current/;}\n' "$location"
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
