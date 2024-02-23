f3814709069f39110decc743533eccc2f76679e2
leoworqu <leoworqu@gmail.com>
2024-02-23 04:43:10 -0800

    dd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,24 +9,22 @@ from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['54.90.31.96', '100.26.156.63']
 
-
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
-
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        filename = archive_path.split('/')[-1]
-        foldername = filename.split('.')[0]
-        release_path = '/data/web_static/releases/{}/'.format(foldername)
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, release_path))
-        run('rm /tmp/{}'.format(filename))
-        run('rm -f /data/web_static/current')
-        run('ln -s {} {}'.format(release_path, '/data/web_static/current'))
-
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    except Exception as e:
+    except:
         return False
