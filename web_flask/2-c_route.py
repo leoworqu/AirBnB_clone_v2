6687237a228c984e15770995919bd3e18db498f0
leoworqu <leoworqu@gmail.com>
2024-02-25 12:09:32 +0000

    route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 """
-This script starts a Flask web application.
-
-The web application listens on 0.0.0.0, port 5000.
-
-Routes:
-    /: Displays "Hello HBNB!"
-    /hbnb: Displays "HBNB"
-    /c/<text>: Displays "C " followed by the value of the text variable 
-               (replace underscore _ symbols with a space)
-
-The option strict_slashes=False is used in route definitions.
+Flask web application with routes for displaying messages.
 """
 
 
@@ -29,6 +19,15 @@ def hbnb():
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Route to display 'C ' followed by the value of the text variable."""
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)ile "<stdin>", line 32
+    
+                                                 ^
+SyntaxError: unexpected EOF while parsing
 def c_text(text):
     """Route to display 'C ' followed by the value of the text variable."""
     return 'C {}'.format(escape(text).replace('_', ' '))
